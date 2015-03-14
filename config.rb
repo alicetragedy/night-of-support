163f0c24c0209790796ff070025649b5a5c906a2
Sebastian Gräßl <sebastian@validcode.me>
2015-03-14 16:39:38 +0100

    correct deployment

--- config.rb
+++ config.rb
@@ -57,8 +57,8 @@ set :images_dir, 'images'
 configure :build do
   activate :minify_css
   activate :minify_javascript
+end
 
-  activate :deploy do |deploy|
-    deploy.method = :git
-  end
+activate :deploy do |deploy|
+  deploy.method = :git
 end
